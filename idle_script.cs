1ea7039add61a5a0127c97555091f3e7de6d086f
Juan Gomez <sebaslogo@gmail.com>
2020-07-24 20:16:44 -0500

    Add files via upload

---
+++ idle_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class idle_script : MonoBehaviour
+{
+
+    base_behavior bb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bb = gameObject.GetComponent<base_behavior>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        UnityEditor.Handles.Label(transform.position + Vector3.up * 3,"Idle");
+    }
+
+
+}
---
+++ seek_script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seek_script : MonoBehaviour
+{
+    base_behavior bb;
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bb = gameObject.GetComponent<base_behavior>();
+        target = bb.target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seekPoint();
+    }
+
+    void seekPoint() //move towards the target
+    {
+        if((target.transform.position - transform.position).magnitude > 10.0f)
+        {
+            transform.position += (target.transform.position - transform.position).normalized * 0.3f;
+        }
+        else
+        {
+            bb.changeState(base_behavior.UnitFSM.Idle);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        UnityEditor.Handles.Label(transform.position + Vector3.up * 3, "Seek");
+    }
+
+}
